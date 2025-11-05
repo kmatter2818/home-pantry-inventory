199123c7c61399f95079737e0fa39d442c369e91
kmatter2818 <kmatter74@gmail.com>
2025-11-05 18:36:56 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "pantry-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return (
+        resp ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        )
+      );
+    })
+  );
+});
